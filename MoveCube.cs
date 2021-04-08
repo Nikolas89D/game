875f8e07c2ee7d6949791c617d48057b6bbab8d6
Nik89 <81176622+Nikolas89D@users.noreply.github.com>
2021-04-07 22:47:57 -0400

    Add files via upload
    Player move and player look basic scripts for use anywhere, made with Brackeys tutorial.

---
+++ MoveCube.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // The reason why FixedUpdate is here is because Unity likes when we use FixedUpdate when we use physics related things in C#
+    void FixedUpdate ()
+    {
+        
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce* Time.deltaTime,0,0);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0,0);
+        }
+    }
+}
---
+++ ScriptFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ScriptFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.position);
+    }
+}
